72fbb13795b3f4bcac43d51069870a102aef3afd
primado <primadoprincepa@gmail.com>
2025-03-07 12:32:13 +0000

    fix: loader component

--- src/components/custom-ui/loader.tsx
+++ src/components/custom-ui/loader.tsx
@@ -1,12 +1,19 @@
+import { Oval } from "react-loader-spinner";
 
 
-const Loader = () => {
+export default function Loader() {
+
     return (
-      <div className="relative w-4 h-4">
-        <div className="absolute w-full h-full border-2  border-white dark:border-custom_theme-dark_gray_2 rounded-full animate-spin"></div>
-      </div>
-    );
-  };
-  
-  export default Loader;
-  
+        <Oval 
+            visible={true}
+            height="20"
+            width="20"
+            strokeWidth={4}
+            color="#fff"
+            ariaLabel="oval-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+        />  
+    )
+    
+};
