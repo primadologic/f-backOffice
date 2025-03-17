2cbc32615203f0c0ac587b344de0e88bdbf43fbc
primado <primadoprincepa@gmail.com>
2025-03-17 23:02:03 +0000

    add files

--- src/features/CaseFiles/tables/detail-view/files.tsx
+++ src/features/CaseFiles/tables/detail-view/files.tsx
@@ -1,28 +1,25 @@
 
 import { ApiResponse } from "@/common/api-response.type";
-import { Button } from "@/components/ui/button";
-import {
-    Collapsible,
-    CollapsibleContent,
-    CollapsibleTrigger,
-} from "@/components/ui/collapsible"
+import { ReportNumberType } from "@/common/Type/ReportNumbers/report-number";
 import { useDetailCaseFile } from "@/hooks/state/case-files/case-file-store";
 import { useRetrieveCaseFileService } from "@/service/case-files/service";
-import { ChevronsUpDown } from "lucide-react";
-import { useState } from "react";
+
 
 
 export default function DetailViewFilesTab() {
 
     const { selectedCaseFile } = useDetailCaseFile();
-
-     const [ open, setOpen ] = useState<boolean>(false)
   
     const caseId: string | null = selectedCaseFile?.caseId ?? ""
 
-    const retrieveCaseFile: ApiResponse = useRetrieveCaseFileService(caseId).data;
- 
-     const reportFiles = retrieveCaseFile?.data?.reports || []
+    const caseFileData: ApiResponse = useRetrieveCaseFileService(caseId).data;
+
+    const caseFileReports: ReportNumberType[] = caseFileData?.data?.reports || []
+    
+    const allReportFiles: string[] = caseFileReports
+    .flatMap((report) => report?.reportFiles || [])  // Flatten all reportFiles
+    .filter((file) => file.startsWith('http')); // Keep only the files that are URLs
+
 
     return (
         <div className="my-10">
@@ -31,36 +28,31 @@ export default function DetailViewFilesTab() {
                    Report Files
                 </label>
                 <div className="">
-                    <Collapsible
-                        open={open}
-                        onOpenChange={setOpen}
-                        className="w-full "
-                    >
-                        <div className="flex items-center justify-between space-x-4 px-4">
+                 
+                        <div className="flex items-center justify-between space-x-4 px-4 py-3">
                             <h4 className="text-sm font-semibold sm:block hidden">
                                 Click to view files
                             </h4>
                             <h4 className="text-sm font-semibold sm:hidden block">
                                 Tap to view files
                             </h4>
-                            <CollapsibleTrigger asChild>
-                                <Button variant="ghost" size="sm">
-                                    <ChevronsUpDown className="h-4 w-4" />
-                                    <span className="sr-only">Toggle</span>
-                                </Button>
-                            </CollapsibleTrigger>
+                            
                         </div>
-                        {reportFiles?.map((file: string, index: number) => (
-                            <CollapsibleContent key={index + 1} className="max-w-max ">
-                                <div className="rounded-md  border  px-4 py-2 font-mono text-sm shadow-sm">
-                                    <a href={file} target="_blank" className=" ">
-                                        <span className="font-bold flex ">Evidence {index + 1}</span>
-                                    </a>
-                                </div>
-                            </CollapsibleContent>
-                        ))}
-                        
-                    </Collapsible>
+
+                        {allReportFiles.length > 0 && (
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"> 
+                                {allReportFiles.map((data, index) => (
+                                    <div
+                                        key={index}
+                                        className="rounded-md border px-4 py-2 font-mono text-sm shadow-sm"
+                                    >
+                                        <a href={data} target="_blank" className="block w-full truncate">Evidence {index + 1}</a>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+
+                
                 </div>
             </div>
         </div>
